29f9e691811b105412125fa792def228a7517eda
RohitAI <55452659+RohitAI@users.noreply.github.com>
2023-02-04 11:46:15 +0530

    Add files via upload

---
+++ rohit-5/func.py
@@ -0,0 +1,8 @@
+def sumpro(a,b):
+    sum=a+b
+    pro=a*b
+    return(sum, pro)
+
+x=int(input())
+y=int(input())
+print(sumpro(x,y))
---
+++ rohit-5/sum.py
@@ -0,0 +1,7 @@
+def sum(*b):
+    c=0
+    for i in b:
+        c+=i
+    print(c)
+sum(2,4,56,78)
+        
